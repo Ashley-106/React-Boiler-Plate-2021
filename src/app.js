ace8cd1e3d45d62c0b82062cbf4091b4188b900d
Ashley <ashleymarsh106@gmail.com>
2021-01-06 19:26:19 +0000

    router update v3

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import './styles/base/base.scss';
 
+import AppRouter from './routers/AppRouter';
+
 const Template = () => {
     return <div>
                     <h1>React Boiler Plate</h1>
             </div>;
-} 
+}; 
 
-reactDOM.render(<Template />, document.getElementById('app'));
+reactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Test = () => {
+    return <div>
+                <h1>React BoilerPlate</h1>
+            </div>;
+};
+
+export default Test;
